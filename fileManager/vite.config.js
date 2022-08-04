5a171e7b407bfd94bf75574559c1ec5f9b91b077
sunkai <sunkai@anming.tech>
2022-08-04 13:41:26 +0800

    智能文件管理

--- fileManager/vite.config.js
+++ fileManager/vite.config.js
@@ -7,47 +7,13 @@ import { join } from 'path';
 function resolve(dir){
   return join(__dirname,dir)
 }
-function wrapperEnv(envConf){
-  const ret = {};
-
-  for (const envName of Object.keys(envConf)) {
-    let realName = envConf[envName].replace(/\\n/g, '\n');
-    realName = realName === 'true' ? true : realName === 'false' ? false : realName;
-
-    if (envName === 'VITE_PORT') {
-      realName = Number(realName);
-    }
-    if (envName === 'VITE_PROXY' && realName) {
-      try {
-        realName = JSON.parse(realName.replace(/'/g, '"'));
-      } catch (error) {
-        realName = '';
-      }
-    }
-    ret[envName] = realName;
-    if (typeof realName === 'string') {
-      process.env[envName] = realName;
-    } else if (typeof realName === 'object') {
-      process.env[envName] = JSON.stringify(realName);
-    }
-  }
-  return ret;
-}
 // https://vitejs.dev/config/
 export default (({
-  command,
   mode
 }) => {
-  console.log(process.cwd(),"cwd")
   const ed = loadEnv(mode, process.cwd());
-  const root = process.cwd();
-  const viteEnv = wrapperEnv(ed);
-  // console.log(viteEnv,"viteEnv")
-  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
-  // console.log(ed.VITE_API_URL, "当前环境变量")
-  return {
-    base: VITE_PUBLIC_PATH,
-    root,
+  console.log(ed.VITE_API_URL, "当前环境变量")
+  return defineConfig({
     plugins: [vue()],
     server: {
       proxy: {
@@ -56,10 +22,8 @@ export default (({
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, '')
         }
-      },
-      
+      }
     },
-    
     define: {
 
     },
@@ -67,10 +31,6 @@ export default (({
       alias:{
         '@':resolve('src')
       }
-    },
-    optimizeDeps:{
-      include:[
-      ]
     }
-  }
+  })
 })
